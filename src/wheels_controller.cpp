66386bb78064de78dff1a978e09fe682c23ea3cc
WillemSelten <WillemSelten>
2015-03-11 15:37:34 +0100

    Small test for motor movement

--- src/wheels_controller.cpp
+++ src/wheels_controller.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/UInt8.h"
+#include "std_msgs/Bool.h"
 
 #include <sstream>
 
@@ -10,20 +11,26 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 
 	ros::Publisher send_pwm = nh.advertise<std_msgs::UInt8>("pwm_value",100);
+	ros::Publisher send_dir = nh.advertise<std_msgs::Bool>("direction",100);
 
 	ros::Rate loop_rate(10);
 	int count = 0;
-
+	bool direct = true;
 	while (ros::ok())
 	{
+		std_msgs::UInt8 value;
+		std_msgs::Bool  dir;
 		if (count == 256)
 		{	
 			count=0;
+			if (direct==true){direct = false;}
+			else {direct = true;}
 		}
-		std_msgs::UInt8 value;
+		
 		value.data = count;
-
+		dir.data = direct;
 		send_pwm.publish(value);
+		send_dir.publish(dir);
 		//ROS_INFO("%s", value.data);
 		ros::spinOnce();
 		loop_rate.sleep();
