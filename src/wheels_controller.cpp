385b1186ec9c3c97c2a15b718e06fbba20bd7868
WillemSelten <WillemSelten>
2015-03-11 18:04:20 +0100

    Modified for 4 motors

--- src/wheels_controller.cpp
+++ src/wheels_controller.cpp
@@ -7,13 +7,20 @@
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "wheels_controller");
-
+	//initializing package
 	ros::NodeHandle nh;
-
-	ros::Publisher send_pwm = nh.advertise<std_msgs::UInt8>("pwm_value",100);
-	ros::Publisher send_dir = nh.advertise<std_msgs::Bool>("direction",100);
-
+	//starting node
+	ros::Publisher send_pwm_motor1 = nh.advertise<std_msgs::UInt8>("pwm_value_motor1",100);
+	ros::Publisher send_pwm_motor2 = nh.advertise<std_msgs::UInt8>("pwm_value_motor2",100);
+	ros::Publisher send_pwm_motor3 = nh.advertise<std_msgs::UInt8>("pwm_value_motor3",100);
+	ros::Publisher send_pwm_motor4 = nh.advertise<std_msgs::UInt8>("pwm_value_motor4",100);
+	ros::Publisher send_dir_motor1 = nh.advertise<std_msgs::Bool>("direction_motor1",100);
+	ros::Publisher send_dir_motor2 = nh.advertise<std_msgs::Bool>("direction_motor2",100);
+	ros::Publisher send_dir_motor3 = nh.advertise<std_msgs::Bool>("direction_motor3",100);
+	ros::Publisher send_dir_motor4 = nh.advertise<std_msgs::Bool>("direction_motor4",100);
+	//creating publishers for pwm value and dir value
 	ros::Rate loop_rate(10);
+	//setting loop frequency to 10Hz
 	int count = 0;
 	bool direct = true;
 	while (ros::ok())
@@ -29,8 +36,14 @@ int main(int argc, char **argv)
 		
 		value.data = count;
 		dir.data = direct;
-		send_pwm.publish(value);
-		send_dir.publish(dir);
+		send_pwm_motor1.publish(value);
+		send_pwm_motor2.publish(value);
+		send_pwm_motor3.publish(value);
+		send_pwm_motor4.publish(value);
+		send_dir_motor1.publish(dir);
+		send_dir_motor2.publish(dir);
+		send_dir_motor3.publish(dir);
+		send_dir_motor4.publish(dir);
 		//ROS_INFO("%s", value.data);
 		ros::spinOnce();
 		loop_rate.sleep();
