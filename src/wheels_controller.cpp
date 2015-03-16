92c501cc1f321d2736ee5ad6f4f180547fbb5156
WillemSelten <WillemSelten>
2015-03-16 09:48:17 +0100

    Added launch file

--- src/wheels_controller.cpp
+++ src/wheels_controller.cpp
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
 	//initializing package
 	ros::NodeHandle nh;
 	//starting node
-	ros::Publisher send_pwm_motor1 = nh.advertise<std_msgs::UInt8>("pwm_value_motor1",100);
+	ros::Publisher send_pwm_motor1 = nh.advertise<std_msgs::UInt8>("pwm_value",100);
 	ros::Publisher send_pwm_motor2 = nh.advertise<std_msgs::UInt8>("pwm_value_motor2",100);
 	ros::Publisher send_pwm_motor3 = nh.advertise<std_msgs::UInt8>("pwm_value_motor3",100);
 	ros::Publisher send_pwm_motor4 = nh.advertise<std_msgs::UInt8>("pwm_value_motor4",100);
